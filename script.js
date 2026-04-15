3bedfbc7740e71f7cfcc092e4ea7cd114936c621
Sahara312 <s.swillens@pm.me>
2026-04-15 19:36:58 +0200

    changes from Feedback-Video done

--- script.js
+++ script.js
@@ -1,174 +1,149 @@
+const dialogRef = document.getElementById("dialog");
+let currentIndex = 0;
 
-    const dialogRef = document.getElementById("dialog");
 
-    const babyGalleryImgs = [
-        "BabyWithMe.jpg",
-        "Basket.jpg",
-        "BigBed.jpg",
-        "Newborn.jpg"
-    ]
+const babyGalleryImgs = [
+  "BabyWithMe.jpg",
+  "Basket.jpg",
+  "BigBed.jpg",
+  "Newborn.jpg",
+];
 
-    const babyNames = [
-        "Baby-Paco mit mir",
-        "Pacos Lieblingsort",
-        "Pacos Bettchen",
-        "Frisch geschlüpft"
-    ]
-    
-    const bigGalleryImgs = [
-        "BeautifulTree.jpg",
-        "BestBuddy.jpg",
-        "BigBoyWithMe.jpg",
-        "Coming.jpg",
-        "Grass.jpg",
-        "HiTHere.jpg",
-        "HugeStick.jpg",
-        "Lake.jpg",
-        "LayingOnTheWall.jpg",
-        "RedRing.jpg",
-        "Relax.jpg",
-        "Stick.jpg",
-        "Tongue.jpg",
-        "TreeTrunk.jpg"
-    ]
+
+const babyNames = [
+  "Baby-Paco mit mir",
+  "Pacos Lieblingsort",
+  "Pacos Bettchen",
+  "Frisch geschlüpft",
+];
+
+
+const bigGalleryImgs = [
+  "BeautifulTree.jpg",
+  "BestBuddy.jpg",
+  "BigBoyWithMe.jpg",
+  "Coming.jpg",
+  "Grass.jpg",
+  "HiTHere.jpg",
+  "HugeStick.jpg",
+  "Lake.jpg",
+  "LayingOnTheWall.jpg",
+  "RedRing.jpg",
+  "Relax.jpg",
+  "Stick.jpg",
+  "Tongue.jpg",
+  "TreeTrunk.jpg",
+];
+
 
 const bigNames = [
-        "Schöner Herbsttag",
-        "Bester Freund",
-        "Wir zusammen",
-        "Ich komme",
-        "Im Gras",
-        "Hi There!",
-        "Ein ganz kleines Stöckchen",
-        "Am See",
-        "Voll am Chillen",
-        "Im Sommer",
-        "Lazy Day",
-        "Meins!",
-        "Ätsch!",
-        "Mein Kunststück"
-    ]
-
-    function renderGallery() {
-
-    let babyGalleryContainer = document.getElementById("babyGalleryContainer");
-    let bigGalleryContainer = document.getElementById("bigGalleryContainer");
+  "Schöner Herbsttag",
+  "Bester Freund",
+  "Wir zusammen",
+  "Ich komme",
+  "Im Gras",
+  "Hi There!",
+  "Ein ganz kleines Stöckchen",
+  "Am See",
+  "Voll am Chillen",
+  "Im Sommer",
+  "Lazy Day",
+  "Meins!",
+  "Ätsch!",
+  "Mein Kunststück",
+];
 
 
-        for (let i = 0; i < babyGalleryImgs.length; i++) {
+function renderGallery() {
+  let babyGalleryContainer = document.getElementById("babyGalleryContainer");
+  let bigGalleryContainer = document.getElementById("bigGalleryContainer");
 
-            let filenameBaby = babyGalleryImgs[i];                  // filename wird hier als Parameter genommen        
+  for (let i = 0; i < babyGalleryImgs.length; i++) {
+    let filenameBaby = babyGalleryImgs[i]; // filename wird hier als Parameter genommen
 
-            babyGalleryContainer.innerHTML += displayGalleryPhotoBaby (i, filenameBaby); // die TemplateFunktion wird ausgelagert.
-        }
+    babyGalleryContainer.innerHTML += displayGalleryPhotoBaby(i, filenameBaby); // die TemplateFunktion wird ausgelagert.
+  }
 
-        for (let i = 0; i < bigGalleryImgs.length; i++) {
-            
-            let filenameBig = bigGalleryImgs[i];
+  for (let i = 0; i < bigGalleryImgs.length; i++) {
+    let filenameBig = bigGalleryImgs[i];
 
-            bigGalleryContainer.innerHTML += displayGalleryPhotoBig (i, filenameBig);
-
-            
-        }
-    }
-
-
-
-    function openDialogBaby(i) {
-        currentIndex = i;
-        let pictureContainer = document.getElementById("pictureContainer");
-        let lastNextButtons = document.getElementById("lastNextButtons");
-        
-        dialogRef.showModal();
-        dialogRef.classList.add("opened");
-
-        pictureContainer.innerHTML = `
-            <h2>${babyNames[i]}</h2>
-            <img class="bigImg" src="img/${babyGalleryImgs[i]}" alt="${babyNames[i]}">
-        `;
+    bigGalleryContainer.innerHTML += displayGalleryPhotoBig(i, filenameBig);
+  }
+}
 
 
-        lastNextButtons.innerHTML = `
-        <button id="prevButton" onclick="prevImgBaby()" class="dialogButton" tabindex="0">
-                <img src="./icon/letztes_default.png" alt="vorheriges Bild">
-            </button>
-            ${displayCounterBaby(i)}
-            <button id="nextButton" onclick="nextImgBaby()" class="dialogButton" tabindex="0">
-                <img src="./icon/next_default.png" alt="nächstes Bild">
-            </button>
-        `
+function openDialogBaby(i) {
+  currentIndex = i;
+  let pictureContainer = document.getElementById("pictureContainer");
+  let lastNextButtons = document.getElementById("lastNextButtons");
 
-    }
+  dialogRef.showModal();
+  dialogRef.classList.add("opened");
 
-    function openDialogBig(i) {
-        currentIndex = i;
-        let pictureContainer = document.getElementById("pictureContainer");
-        let lastNextButtons = document.getElementById("lastNextButtons");
+  pictureContainer.innerHTML = displayPictureContainerBaby(i);
 
-        dialogRef.showModal();
-        dialogRef.classList.add("opened");
-
-        pictureContainer.innerHTML = `
-            <h2>${bigNames[i]}</h2>
-            <img class="bigImg" src="img/${bigGalleryImgs[i]}" alt="${bigNames[i]}">
-        `;
-
-        lastNextButtons.innerHTML = `
-        <button id="prevButton" onclick="prevImgBig()" class="dialogButton" tabindex="0">
-                <img src="./icon/letztes_default.png" alt="vorheriges Bild">
-            </button>
-            ${displayCounterBig(i)}
-            <button id="nextButton" onclick="nextImgBig()" class="dialogButton" tabindex="0">
-                <img src="./icon/next_default.png" alt="nächstes Bild">
-            </button>
-        `
-
-    }
-
-    function closeDialog() {
-        dialogRef.close();
-        dialogRef.classList.remove("opened");
-    }
+  lastNextButtons.innerHTML = displayLastNextButtonsBaby(i);
+  
+}
 
 
-    let currentIndex = 0;
+function openDialogBig(i) {
+  currentIndex = i;
+  let pictureContainer = document.getElementById("pictureContainer");
+  let lastNextButtons = document.getElementById("lastNextButtons");
 
-    function prevImgBaby () {
-        currentIndex--;
+  dialogRef.showModal();
+  dialogRef.classList.add("opened");
 
-        if (currentIndex < 0) {
-            currentIndex = babyGalleryImgs.length - 1;
-        }
+  pictureContainer.innerHTML = displayPictureContainerBig(i);
 
-        openDialogBaby(currentIndex);
-    }
+  lastNextButtons.innerHTML = displayLastNextButtonsBig(i);
+}
 
-    function nextImgBaby () {
-        currentIndex ++;
 
-        if (currentIndex >= babyGalleryImgs.length) {
-            currentIndex = 0;
-        }
-        openDialogBaby(currentIndex);
-    }
+function closeDialog() {
+  dialogRef.close();
+  dialogRef.classList.remove("opened");
+}
 
-    function prevImgBig () {
-        currentIndex--;
 
-        if (currentIndex < 0) {
-            currentIndex = bigGalleryImgs.length - 1;
-        }
+function prevImgBaby() {
+  currentIndex--;
 
-        openDialogBig(currentIndex);
-    }
+  if (currentIndex < 0) {
+    currentIndex = babyGalleryImgs.length - 1;
+  }
 
-    function nextImgBig () {
-        currentIndex ++;
+  openDialogBaby(currentIndex);
+}
 
-        if (currentIndex >= bigGalleryImgs.length) {
-            currentIndex = 0;
-        }
-        openDialogBig(currentIndex);
-    }
 
-    
+function nextImgBaby() {
+  currentIndex++;
+
+  if (currentIndex >= babyGalleryImgs.length) {
+    currentIndex = 0;
+  }
+  openDialogBaby(currentIndex);
+}
+
+
+function prevImgBig() {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = bigGalleryImgs.length - 1;
+  }
+
+  openDialogBig(currentIndex);
+}
+
+
+function nextImgBig() {
+  currentIndex++;
+
+  if (currentIndex >= bigGalleryImgs.length) {
+    currentIndex = 0;
+  }
+  openDialogBig(currentIndex);
+}
--- template.js
+++ template.js
@@ -1,33 +1,73 @@
-function displayGalleryPhotoBaby (i, filenameBaby) {            // das ist die Template-FUnktion, die meine BabyImgs in die Gallerie rendert
-    return `
+function displayGalleryPhotoBaby(i, filenameBaby) {
+  // das ist die Template-FUnktion, die meine BabyImgs in die Gallerie rendert
+  return `
             <li>
                 <button onclick="openDialogBaby(${i})">
                 <img src="./img/${filenameBaby}" 
                 alt="Bild von Baby-Paco">
                 </button>
             </li>
-            `                               // die Funktion openDialogBaby(i) wird auf das Thumbnail gesatzt, das dann das Bild mit dem Namen auf dem filenameBaby = babyGalleryImgs[i]- Array einfügt!
+            `; // die Funktion openDialogBaby(i) wird auf das Thumbnail gesatzt, das dann das Bild mit dem Namen auf dem filenameBaby = babyGalleryImgs[i]- Array einfügt!
 }
 
-function displayGalleryPhotoBig (i, filenameBig) {
-    return `
+function displayGalleryPhotoBig(i, filenameBig) {
+  return `
             <li>
                 <button onclick="openDialogBig(${i})">
                 <img src="./img/${filenameBig}" 
                 alt="Bild von Paco, erwachsen">
                 </button>
             </li>
-            `
+            `;
 }
 
 function displayCounterBaby(i) {
-    return  `
-            ${i+1} von ${babyGalleryImgs.length}
-            `
+  return `
+            ${i + 1} von ${babyGalleryImgs.length}
+            `;
 }
 
 function displayCounterBig(i) {
-    return  `
-            ${i+1} von ${bigGalleryImgs.length}
-            `
+  return `
+            ${i + 1} von ${bigGalleryImgs.length}
+            `;
+}
+
+function displayPictureContainerBig(i) {
+  return `
+            <h2>${bigNames[i]}</h2>
+            <img class="bigImg" src="img/${bigGalleryImgs[i]}" alt="${bigNames[i]}">
+        `;
+}
+
+function displayPictureContainerBaby(i) {
+  return `
+            <h2>${babyNames[i]}</h2>
+            <img class="bigImg" src="img/${babyGalleryImgs[i]}" alt="${babyNames[i]}">
+        `;
+}
+
+
+function displayLastNextButtonsBaby(i) {
+    return `
+            <button id="prevButton" onclick="prevImgBaby()" class="dialogButton" tabindex="0">
+                <img src="./icon/letztes_default.png" alt="vorheriges Bild">
+            </button>
+            ${displayCounterBaby(i)}
+            <button id="nextButton" onclick="nextImgBaby()" class="dialogButton" tabindex="0">
+                <img src="./icon/next_default.png" alt="nächstes Bild">
+            </button>
+        `;
+}
+
+function displayLastNextButtonsBig(i){
+return `
+        <button id="prevButton" onclick="prevImgBig()" class="dialogButton" tabindex="0">
+                <img src="./icon/letztes_default.png" alt="vorheriges Bild">
+            </button>
+            ${displayCounterBig(i)}
+            <button id="nextButton" onclick="nextImgBig()" class="dialogButton" tabindex="0">
+                <img src="./icon/next_default.png" alt="nächstes Bild">
+            </button>
+        `;
 }
